b864c2979ede3d5decbc43ea768d800d9c1eec3f
hillewaert <hillewaert.daniel@outlook.com>
2019-12-12 19:45:21 +0100

    001

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 
 <template>
-
     <div>
-        <Header></Header>
-        <Main></Main>
+        <Connexion></Connexion>
+        <Client></Client>
     </div>
 </template>
 
 <script>
+    import Connexion from "@/components/Connexion"
+    import Client from "@/components/Client"
 
+    export default {
+        name: 'App',	// nom parent
+        components:{
+            Connexion,
+            Client,
+        }
+    }
 </script>
 
 <style>
---
+++ src/components/Client.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        ...Template <strong>Client</strong>...
+    </div>
+</template>
+<script>
+    export default {
+        name: "Client",
+        props:[],
+    }
+</script>
+<style scoped>
+</style>
