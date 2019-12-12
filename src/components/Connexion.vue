500ae3d5bdd69da88c468354147f02e5a5b1e538
Unknown <unknown@example.com>
2019-12-12 20:46:16 +0100

    005

--- src/components/Connexion.vue
+++ src/components/Connexion.vue
@@ -1,13 +1,22 @@
 <template>
+  <div>
     <div>
-        ...Template <strong>Connexion</strong>...
+      <h1>Connexion</h1>
     </div>
+    <form action="" method="">
+        <input type="text" id="name" name="user_name" placeholder="user">
+        <input type="text" id="mail" name="user_mail" placeholder="mot de passe">
+        <input type="submit">
+    </form
+  </div>
 </template>
+
 <script>
-    export default {
-        name: "Connexion",
-        props:[],
-    }
+  export default {
+  name: "Connexion",
+  
+  }
 </script>
-<style scoped>
+
+<style>
 </style>
