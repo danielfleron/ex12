a651eb7d27f36048093a9f9006e642d0c2c77459
hillewaert <hillewaert.daniel@outlook.com>
2019-12-12 20:35:21 +0100

    00

---
+++ src/components/Connexion.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        ...Template <strong>Connexion</strong>...
+    </div>
+</template>
+<script>
+    export default {
+        name: "Connexion",
+        props:[],
+    }
+</script>
+<style scoped>
+</style>
